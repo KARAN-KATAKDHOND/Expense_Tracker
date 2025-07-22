2fc8b9ecbaf1a03abfe82193a3674bed0408e6e3
KARAN NANA KATAKDHOND <karankatakdhond23@gmail.com>
2025-07-22 21:14:38 +0530

    Trial for expenses.dart

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -3,27 +3,29 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense_model.dart';
 import 'package:expense_tracker/widgets/expenses_list/expense_item_widget.dart';
-class Expenses extends StatefulWidget {
-    const Expenses({super.key});
 
-    @override
-    State<Expenses> createState(){
-        return _ExpenseState();
-    }
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpenseState();
+  }
 }
-class _ExpenseState extends State<Expenses>{
-    
-    final List<Expense> _registeredExpenses = [
-        Expense(amount: 19.99,
-        title: 'Flutter Course', 
-        date: DateTime.now(), 
-        category: Category.work,
-        ),
-        Expense(
-        amount: 22.22 ,
-        title: 'Mangrove Cleaning Auto Fare',
-        date: DateTime.now(),
-        category: Category.travel,
+
+class _ExpenseState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      amount: 19.99,
+      title: 'Flutter Course',
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      amount: 22.22,
+      title: 'Mangrove Cleaning Auto Fare',
+      date: DateTime.now(),
+      category: Category.travel,
     ),
     Expense(
       amount: 349.00,
@@ -37,17 +39,35 @@ class _ExpenseState extends State<Expenses>{
       date: DateTime.now(),
       category: Category.food,
     ),
-    ];
-    
-    @override 
-    Widget build (BuildContext context){
-        return MaterialApp(
+  ];
+
+void _openAddExpenseOverlay(){
+  showModalBottomSheet(context: context, builder: builder))
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          
+          title: Center(child: Text('Expense Tracker',textAlign: TextAlign.center,)),
+          actions: [
+            IconButton(
+            onPressed: () {},
+             icon: const Icon(Icons.add)),
+          ],
+        ),
         body: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
-              colors: [const Color.fromARGB(255, 34, 155, 254), const Color.fromARGB(255, 110, 161, 249)],
+              colors: [
+                const Color.fromARGB(255, 34, 155, 254),
+                const Color.fromARGB(255, 110, 161, 249),
+              ],
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
             ),
@@ -57,12 +77,13 @@ class _ExpenseState extends State<Expenses>{
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisSize: MainAxisSize.max,
             children: [
-                Text('The Chart'),
-                Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+              //Toolbar with the add button => Row()
+              Text('The Chart'),
+              Expanded(child: ExpensesList(expenses: _registeredExpenses)),
             ],
-          )
+          ),
         ),
       ),
     );
-    }
+  }
 }
